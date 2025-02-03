c00bb5e24786016896b703ad640a116a72ee6a43
mvdgp <mvandraanen@hotmail.com>
2025-02-03 19:54:37 +0100

    tailwind content expand

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,8 @@
 const tailwindConfig = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {

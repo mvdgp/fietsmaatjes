df29acc068c3a7705552522f5f229fc8a2abb2c4
mvdgp <mvandraanen@hotmail.com>
2025-04-04 19:36:16 +0200

    Added Fietsknoop Handleiding button

--- src/app/bike-route.jsx
+++ src/app/bike-route.jsx
@@ -13,11 +13,15 @@ const BikeRoute = ({ data }) => {
                     <div className="flex flex-col lg:flex-row justify-between">
                         <div className="flex flex-col gap-2 justify-center">
                             <h3 className="pt-1">{data.title}</h3>
-                            <div className="">
+                            <div className="flex flex-col gap-3">
                                 <p className="font-bold">Afstand: {data.distance}km</p>
-                                <div className="mt-4 flex flex-col gap-1">
-                                    <button className="py-2 w-[250px]" onClick={() => window.location.href = data.download_route.url}>Download Fietsknoop[+] Route</button>
-                                </div>
+                                <a
+                                    href="/Handleiding-Fietsknoop.pdf"
+                                    download="Handleiding-Fietsknoop.pdf"
+                                    className="font-bold px-3 py-2 w-[250px] text-center bg-none text-primary rounded-lg border border-primary hover:bg-primary hover:text-tertiary hover:no-underline"
+                                >
+                                    Handleiding Fietsknoop App
+                                </a>
                             </div>
                         </div>
                         <PrismicImage
@@ -28,9 +32,10 @@ const BikeRoute = ({ data }) => {
                     </div>
                 </div>
                 <p className="mt-4 pt-4 border-t border-primary text-justify">{data.body}</p>
-                <br/>
-                <p className="text-justify italic">Scan de QR code of ga naar <a href={data.route_link.url} target="blank">{data.route_link.url}</a><br />
-                    Je kunt de route ook downloaden via de download knop (hiervoor is een Fietsknoop[+] abonnement vereist).</p>
+                <br />
+                <p className="text-justify italic">Scan de QR code of ga naar:<br />
+                    <a href={data.route_link.url} target="blank">{data.route_link.url}</a>
+                </p>
             </div>
             {/* Container for the map image */}
             <a className="w-full" href={data.route_link.url} target="blank">
